6c3d770158b93ad75c741fda1aeb19aaa39a0af4
kinjalbshah@gmail.com <kinjalbshah@gmail.com>
2015-12-30 22:16:47 +0530

    Updated Factory patern to remove config function. Made it simple.

--- FactoryTest/TDS.cs
+++ FactoryTest/TDS.cs
@@ -20,18 +20,13 @@ namespace TDS
         private string _SerialReference;
         private string _uri;
 
-        public Serial_Shipping_Container_Code()
+        public Serial_Shipping_Container_Code(string CompanyPrefix, string SerialReference)
         {   //test checkin - check out
             _EPC_Scheme = "sscc";
-        }
-        public void Config(string CompanyPrefix, string SerialReference)
-        {
             _CompanyPrefix = CompanyPrefix;
             _SerialReference = SerialReference;
-            
-            
         }
-
+        
         public string uri()
         {
             _uri= "urn:epc:id:"+_EPC_Scheme+"." + _CompanyPrefix + "." + _SerialReference;
@@ -52,19 +47,14 @@ namespace TDS
         private string _LocationReference;
         private string _Extension;
         private string _uri;
-        public Global_Location_Number() {
+        public Global_Location_Number(string CompanyPrefix, string LocationReference, string Extension) {
             _EPC_Scheme = "sgln";
-        }
-
-        public void Config(string CompanyPrefix, string LocationReference,string Extension)
-        {
             _CompanyPrefix = CompanyPrefix;
             _LocationReference = LocationReference;
             _Extension = Extension;
-
-
         }
 
+       
         public string uri()
         {
             _uri = "urn:epc:id:" + _EPC_Scheme +":"+ _CompanyPrefix + "." + _LocationReference+"."+_Extension;
@@ -74,35 +64,7 @@ namespace TDS
         
     }
 
-    /// <summary>
-    /// The Creator Abstract Class
-    /// </summary>
-    public abstract class EpcFactory
-    {
-        public abstract IEPC GetEpc(string Epc);
-
-    }
-
-    /// <summary>
-    /// A 'ConcreteCreator' class
-    /// </summary>
-    public class ConcreteEpcFactory : EpcFactory
-    {
-        public override IEPC GetEpc(string Epc)
-        {
-            switch (Epc)
-            {
-                case "Serial_Shipping_Container_Code":
-                    return new Serial_Shipping_Container_Code();
-
-                case "Global_Location_Number":
-                    return new Global_Location_Number();
-                default:
-                    throw new ApplicationException(string.Format("Epc '{0}' cannot be created", Epc));
-            }
-        }
-
-    }
+   
 
     /// <summary>
     /// Factory Pattern 
@@ -111,22 +73,18 @@ namespace TDS
     {
         static void Main(string[] args)
         {
-            EpcFactory factory = new ConcreteEpcFactory();
 
-            /*IEPC SSCC = factory.GetEpc("Serial_Shipping_Container_Code");
+
+            //Serial_Shipping_Container_Code ssc2 =(TDS.Serial_Shipping_Container_Code) factory.GetEpc("Serial_Shipping_Container_Code");
+            Serial_Shipping_Container_Code ssc2 = new Serial_Shipping_Container_Code("0614141", "1234567890");
             
-            string str= SSCC.ToString();
-            Type Obj= SSCC.GetType();
-            SSCC.uri();
-            */
-
-            Serial_Shipping_Container_Code ssc2 =(TDS.Serial_Shipping_Container_Code) factory.GetEpc("Serial_Shipping_Container_Code");
-            ssc2.Config("0614141", "1234567890");
             string Shipping_Final_URI= ssc2.uri();
+            string str = ssc2.ToString(); 
             Type Obj = ssc2.GetType();
 
-            TDS.Global_Location_Number SGLN_Object=(TDS.Global_Location_Number) factory.GetEpc("Global_Location_Number");
-            SGLN_Object.Config("0614141", "12345", "400");
+            TDS.Global_Location_Number SGLN_Object = new Global_Location_Number("0614141", "12345", "400");
+            Type Obj1 = SGLN_Object.GetType();
+            string str1 = SGLN_Object.ToString(); 
             SGLN_Object.uri();
             
             Console.ReadKey();
