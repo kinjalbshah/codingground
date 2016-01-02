8d45db75c64bcd729a31db6747579a526790bd26
kinjalbshah@gmail.com <kinjalbshah@gmail.com>
2016-01-02 16:03:14 +0530

    Implemented SGLN Chep 6.0

--- FactoryTest/TDS.cs
+++ FactoryTest/TDS.cs
@@ -12,7 +12,7 @@ namespace TDS
     public interface IEPC : ISerializable
     {
         string uri();
-        void  save(string filename);
+        void  uri_save(string filename);
          
     }
 
@@ -58,7 +58,7 @@ namespace TDS
             
             _uri= (String)info.GetValue("URI", typeof(string));
         }
-        public void  save(string filename){
+        public void  uri_save(string filename){
             Stream stream = File.Open(filename, FileMode.Create);
             BinaryFormatter bformatter = new BinaryFormatter();
             bformatter.Serialize(stream, this);
@@ -67,6 +67,54 @@ namespace TDS
 
     }
 
+    [Serializable()]
+    public class Serialized_Global_Trade_Item_Number : IEPC
+    {
+        private string _EPC_Scheme;
+        private string _CompanyPrefix;
+        private string _ItemRef;
+        private string _Serial;
+        private string _uri;
+        public Serialized_Global_Trade_Item_Number(string companyprefix, string itemref, string serial)
+        {
+            _EPC_Scheme = "sgtin";
+            _CompanyPrefix = companyprefix;
+            _ItemRef = itemref;
+            _Serial = serial;
+        }
+
+
+        public string uri()
+        {
+            _uri = "urn:epc:id:" + _EPC_Scheme + ":" + _CompanyPrefix + "." + _ItemRef + "." + _Serial;
+            Console.WriteLine("uri of the Serialized_Global_Trade_Item_Number : " + _uri);
+            return _uri;
+        }
+
+        //Serialization function.
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            //You can use any custom name for your name-value pair. But make sure you
+            // read the values with the same name. For ex:- If you write EmpId as "EmployeeId"
+            // then you should read the same with "EmployeeId"
+            info.AddValue("URI", _uri);
+        }
+
+        public Serialized_Global_Trade_Item_Number(SerializationInfo info, StreamingContext ctxt)
+        {
+            //Get the values from info and assign them to the appropriate properties
+
+            _uri = (String)info.GetValue("URI", typeof(string));
+        }
+        public void uri_save(string filename)
+        {
+            Stream stream = File.Open(filename, FileMode.OpenOrCreate);
+            BinaryFormatter bformatter = new BinaryFormatter();
+            bformatter.Serialize(stream, this);
+            stream.Close();
+        }
+
+    }
     /// <summary>
     /// A 'ConcreteProduct' class
     /// </summary>
@@ -79,7 +127,7 @@ namespace TDS
         private string _Serial;
         private string _uri;
         public Aerospace_Defense_ID(string code, string part, string serial) {
-            _EPC_Scheme = "ADI";
+            _EPC_Scheme = "adi";
             _Code =  code;
             _Part = part;
             _Serial = serial;
@@ -108,7 +156,7 @@ namespace TDS
 
             _uri = (String)info.GetValue("URI", typeof(string));
         }
-        public void  save(string filename){
+        public void  uri_save(string filename){
             Stream stream = File.Open(filename, FileMode.Create);
             BinaryFormatter bformatter = new BinaryFormatter();
             bformatter.Serialize(stream, this);
@@ -127,7 +175,7 @@ namespace TDS
         static void Main(string[] args)
         {
 
-
+            /*
             //Serial_Shipping_Container_Code ssc2 =(TDS.Serial_Shipping_Container_Code) factory.GetEpc("Serial_Shipping_Container_Code");
             Serial_Shipping_Container_Code ssc2 = new Serial_Shipping_Container_Code("0614141", "1234567890");
             
@@ -142,7 +190,7 @@ namespace TDS
             // If you use custom extensions, then the user will now 
             //   that the file is associated with your program.
 
-            ssc2.save("TDS.txt");
+            ssc2.uri_save("TDS.txt");
 
             //Deserialize the values by reading it from the file
             //Open the file written above and read values from it.
@@ -155,12 +203,20 @@ namespace TDS
 
             
             Console.WriteLine("URI from Deserialize", ssc2.uri());
+            */
 
-            TDS.Aerospace_Defense_ID SGLN_Object = new Aerospace_Defense_ID("W81X9C", "3KL984PX1", "2WMA52");
+            TDS.Serialized_Global_Trade_Item_Number   SGLN_Object = new Serialized_Global_Trade_Item_Number("0614141", "112345", "400");
             Type Obj1 = SGLN_Object.GetType();
-            string str1 = SGLN_Object.ToString(); 
+            string str1 = SGLN_Object.ToString();
             SGLN_Object.uri();
-            SGLN_Object.save("ADI.txt");
+            SGLN_Object.uri_save("SGLN.txt");
+            Console.ReadKey();
+
+            TDS.Aerospace_Defense_ID ADI_Object = new Aerospace_Defense_ID("W81X9C", "3KL984PX1", "2WMA52");
+            Type Obj2 = ADI_Object.GetType();
+            string str2 = ADI_Object.ToString();
+            ADI_Object.uri();
+            ADI_Object.uri_save("ADI.txt");
             Console.ReadKey();
 
         }
